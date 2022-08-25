ded2889be5faeeb692a28158f63f50753e042b43
Helder Costa <heldercostaaa@gmail.com>
2022-08-25 17:41:40 -0300

    :sparkles: Remove hello route :sparkles: Add generateCertificate route

---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,39 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  await document
+    .put({
+      TableName: "user_certificates",
+      Item: {
+        id,
+        name,
+        grade,
+        created_at: new Date().getTime(),
+      },
+    })
+    .promise();
+
+  const response = await document
+    .query({
+      TableName: "user_certificates",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(response.Items[0]),
+  };
+};
